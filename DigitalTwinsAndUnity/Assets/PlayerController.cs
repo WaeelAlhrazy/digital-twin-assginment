9e00e3919925feb72faed5a57b8605d01dbb62a0
WaeelAlhrazy <wael_abbas11@hotmail.com>
2021-04-17 14:59:20 +0200

    changed the road object name

--- DigitalTwinsAndUnity/Assets/PlayerController.cs
+++ DigitalTwinsAndUnity/Assets/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Camera cam;
+   
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit;
+            
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestintion(hit.point);
+            }
+        }
         
     }
 }
