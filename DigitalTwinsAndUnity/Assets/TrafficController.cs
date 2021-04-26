5c0621949673e9e2d24db49de6633d7e7eaa3a38
WaeelAlhrazy <wael_abbas11@hotmail.com>
2021-04-27 00:09:22 +0200

    editing the traffic lights

--- DigitalTwinsAndUnity/Assets/TrafficController.cs
+++ DigitalTwinsAndUnity/Assets/TrafficController.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TrafficController : MonoBehaviour
 {
+   
+   // public Transform entryA;
+
+    public GameObject RedLight;
+    public GameObject Greenlight;
+    public GameObject YellowLight;
+
+   // private NavMeshAgent agentA;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ControlVehicleMovements();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ControlVehicleMovements()
+    {
+        if (RedLight == true)
+        {
+            GetComponent<Collider>().enabled = false;
+        }
+        else if(Greenlight == true)
+        {
+            GetComponent<Collider>().enabled = true;
+        }
     }
 }
